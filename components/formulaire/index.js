75d19d9811a9a36cb1d73aa90ccaa39fa9d8e0ab
Houssem Hammami <hammamihossem99@gmail.com>
2022-04-03 22:26:18 +0100

    fix formulairez button

--- components/formulaire/index.js
+++ components/formulaire/index.js
@@ -130,7 +130,7 @@ export default function Formulaire() {
                   type="submit"
                   className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Submit
+                  Ajouter cette annonce
                 </button>
               </div>
             </form>
