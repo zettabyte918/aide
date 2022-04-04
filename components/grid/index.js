41e5e084ddc7140b6ddeb08366a7ac8944ca42ba
Houssem Hammami <hammamihossem99@gmail.com>
2022-04-04 19:36:18 +0100

    up

--- components/grid/index.js
+++ components/grid/index.js
@@ -114,7 +114,7 @@ export default function Grid() {
         </div>
         <ul
           role="list"
-          className="py-6 px-6 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+          className="py-6 px-2 lg:px-6 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
         >
           {annonces.map((annonce) => (
             <li key={annonce.source} className="relative">
